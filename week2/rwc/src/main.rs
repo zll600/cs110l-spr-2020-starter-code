86bd99c665119bb12f362ebc5ff063c3b0cfece4
zll600 <3400692417@qq.com>
2022-10-24 08:32:25 +0800

    finsih rwc homework

--- week2/rwc/src/main.rs
+++ week2/rwc/src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
 use std::process;
 
 fn main() {
@@ -8,5 +10,25 @@ fn main() {
         process::exit(1);
     }
     let filename = &args[1];
+
     // Your code here :)
+
+    let file = File::open(filename).expect("Invalid file name");
+    let mut lines_cnt = 0;
+    let mut letters_cnt = 0;
+    let mut words_cnt = 0;
+    for line in io::BufReader::new(file).lines() {
+        let line_str = line.unwrap();
+        println!("lines: {:?}", line_str);
+        lines_cnt += 1;
+        for letter in line_str.chars().map(|c| c.to_string()) {
+            if letter == " " {
+                words_cnt += 1;
+            } else {
+                letters_cnt += 1;
+            }
+        }
+        words_cnt += 1;
+    }
+    println!("lines: {lines_cnt}, words: {words_cnt}, letters: {letters_cnt}");
 }
