f89e864d155b39956a4b3f6a8c560a417dd164bc
zll600 <3400692417@qq.com>
2022-10-25 09:19:11 +0800

    finish week3 linkedlist

--- week3/linked_list/src/linked_list.rs
+++ week3/linked_list/src/linked_list.rs
@@ -1,60 +1,80 @@
 use std::fmt;
 use std::option::Option;
 
-pub struct LinkedList {
-    head: Option<Box<Node>>,
+pub struct LinkedList<T> {
+    head: Option<Box<Node<T>>>,
     size: usize,
 }
 
-struct Node {
-    value: u32,
-    next: Option<Box<Node>>,
+struct Node<T> {
+    value: T,
+    next: Option<Box<Node<T>>>,
 }
 
-impl Node {
-    pub fn new(value: u32, next: Option<Box<Node>>) -> Node {
-        Node {value: value, next: next}
+impl<T> Node<T> {
+    pub fn new(value: T, next: Option<Box<Node<T>>>) -> Node<T> {
+        Node {
+            value: value,
+            next: next,
+        }
     }
 }
 
-impl LinkedList {
-    pub fn new() -> LinkedList {
-        LinkedList {head: None, size: 0}
+impl<T: Clone> Clone for Node<T> {
+    fn clone(&self) -> Self {
+        Node {
+            value: self.value.clone(),
+            next: self.next.clone(),
+        }
     }
-    
+}
+
+impl<T: PartialEq> PartialEq for Node<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.value == other.value && self.next == other.next
+    }
+}
+
+impl<T> LinkedList<T> {
+    pub fn new() -> LinkedList<T> {
+        LinkedList {
+            head: None,
+            size: 0,
+        }
+    }
+
     pub fn get_size(&self) -> usize {
         self.size
     }
-    
+
     pub fn is_empty(&self) -> bool {
         self.get_size() == 0
     }
-    
-    pub fn push_front(&mut self, value: u32) {
-        let new_node: Box<Node> = Box::new(Node::new(value, self.head.take()));
+
+    pub fn push_front(&mut self, value: T) {
+        let new_node: Box<Node<T>> = Box::new(Node::new(value, self.head.take()));
         self.head = Some(new_node);
         self.size += 1;
     }
-    
-    pub fn pop_front(&mut self) -> Option<u32> {
-        let node: Box<Node> = self.head.take()?;
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        let node: Box<Node<T>> = self.head.take()?;
         self.head = node.next;
         self.size -= 1;
         Some(node.value)
     }
 }
 
-
-impl fmt::Display for LinkedList {
+impl<T: fmt::Display> fmt::Display for LinkedList<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut current: &Option<Box<Node>> = &self.head;
+        let mut current: &Option<Box<Node<T>>> = &self.head;
         let mut result = String::new();
         loop {
             match current {
                 Some(node) => {
                     result = format!("{} {}", result, node.value);
                     current = &node.next;
-                },
+                }
                 None => break,
             }
         }
@@ -62,7 +82,7 @@ impl fmt::Display for LinkedList {
     }
 }
 
-impl Drop for LinkedList {
+impl<T> Drop for LinkedList<T> {
     fn drop(&mut self) {
         let mut current = self.head.take();
         while let Some(mut node) = current {
@@ -71,5 +91,17 @@ impl Drop for LinkedList {
     }
 }
 
+impl<T: Clone> Clone for LinkedList<T> {
+    fn clone(&self) -> Self {
+        LinkedList {
+            head: self.head.clone(),
+            size: self.size,
+        }
+    }
+}
 
-
+impl<T: PartialEq> PartialEq for LinkedList<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.get_size() == other.get_size() && self.head == other.head
+    }
+}
--- week3/linked_list/src/main.rs
+++ week3/linked_list/src/main.rs
@@ -8,12 +8,21 @@ fn main() {
     for i in 1..12 {
         list.push_front(i);
     }
+
     println!("{}", list);
     println!("list size: {}", list.get_size());
     println!("top element: {}", list.pop_front().unwrap());
     println!("{}", list);
     println!("size: {}", list.get_size());
     println!("{}", list.to_string()); // ToString impl for anything impl Display
+
+    let mut str_list: LinkedList<String> = LinkedList::new();
+    str_list.push_front(String::from("hello"));
+    str_list.push_front(String::from("world"));
+    println!("{}", str_list);
+    let str_list_clone = str_list.clone();
+    println!("{}", str_list_clone);
+    println!("is same: {}", str_list == str_list_clone);
 
     // If you implement iterator trait:
     //for val in &list {
