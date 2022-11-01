c14e18fe6aa8627af1283ae9f98c5ea77292f1c8
zll600 <3400692417@qq.com>
2022-11-01 12:50:37 +0800

    finish week-5 farm

--- week5/farm/src/main.rs
+++ week5/farm/src/main.rs
@@ -66,17 +66,43 @@ fn get_input_numbers() -> VecDeque<u32> {
     numbers
 }
 
+fn pop_front_from_queue(nums: &mut Arc<Mutex<VecDeque<u32>>>) -> Option<u32> {
+    let mut num_ref = nums.lock().unwrap();
+    return num_ref.pop_front();
+}
+
+fn worker_thread(mut nums: Arc<Mutex<VecDeque<u32>>>) {
+    loop {
+        if let Some(num) = pop_front_from_queue(&mut nums) {
+            factor_number(num)
+        } else {
+            break;
+        }
+    }
+}
+
 fn main() {
     let num_threads = num_cpus::get();
     println!("Farm starting on {} CPUs", num_threads);
     let start = Instant::now();
 
     // TODO: call get_input_numbers() and store a queue of numbers to factor
+    let nums: Arc<Mutex<VecDeque<u32>>> = Arc::new(Mutex::new(get_input_numbers()));
 
     // TODO: spawn `num_threads` threads, each of which pops numbers off the queue and calls
     // factor_number() until the queue is empty
+    let mut threads = Vec::with_capacity(num_threads);
+    for _ in 0..num_threads {
+        let nums_clone = nums.clone();
+        threads.push(thread::spawn(move || {
+            worker_thread(nums_clone);
+        }));
+    }
 
     // TODO: join all the threads you created
+    for handler in threads {
+        handler.join().expect("Error in joining thread");
+    }
 
     println!("Total execution time: {:?}", start.elapsed());
 }
