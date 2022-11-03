d426b11e69689ce9d10e312e38326c07c7442a27
zll600 <3400692417@qq.com>
2022-11-04 07:29:13 +0800

    finish week6 parallel_map

--- week6/parallel_map/src/main.rs
+++ week6/parallel_map/src/main.rs
@@ -8,7 +8,43 @@ where
     U: Send + 'static + Default,
 {
     let mut output_vec: Vec<U> = Vec::with_capacity(input_vec.len());
+    output_vec.resize_with(input_vec.len(), Default::default);
     // TODO: implement parallel map!
+    let (sender, receiver) = crossbeam_channel::unbounded();
+    let (out_sender, out_receiver) = crossbeam_channel::unbounded();
+
+    let mut threads = Vec::new();
+    for _ in 0..num_threads {
+        let receiver_clone = receiver.clone();
+        let out_sender_clone = out_sender.clone();
+        threads.push(thread::spawn(move || {
+            while let Ok(pair) = receiver_clone.recv() {
+                let (val, idx) = pair;
+                out_sender_clone
+                    .send((f(val), idx))
+                    .expect("Tried writint to channel, but there are no out_receivers!");
+            }
+        }));
+    }
+
+    let len = input_vec.len();
+    for i in 0..len {
+        sender
+            .send((input_vec.pop().unwrap(), len - i - 1))
+            .expect("Tried writing to channel, but there are no receivers!");
+    }
+    drop(sender);
+    drop(out_sender);
+
+    while let Ok(pair) = out_receiver.recv() {
+        let (val, idx) = pair;
+        output_vec[idx] = val;
+    }
+
+    for thread in threads {
+        thread.join().expect("Panic occured in thread");
+    }
+
     output_vec
 }
 
